c352b0a12c8aa23058001d51562e2272f015297e
gponciano <gab_bash@hotmail.com>
2025-10-22 07:52:06 +1100

    Adding Pricing component and video component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import Header from "./components/landing-page/Header";
 import Hero from "./components/landing-page/Hero";
+import Pricing from "./components/landing-page/Pricing";
+import VideoExplanation from "./components/landing-page/Video-Explanation";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header/>
       <Hero/>
-      {/* 
-      <Hero/
+
       <VideoExplanation/>
       <Pricing/>
-      <FAQ/> */}
+      {/* <FAQ/> */}
     </div>
   );
 }
